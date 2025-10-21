1cb3c2398b412d344a007f83b4c73c521bc59e13
tbrendanj <tbrendanj@gmail.com>
2025-10-22 03:17:39 +0900

    simple middleware

---
+++ nextjs/src/middleware.ts
@@ -0,0 +1,16 @@
+// ./frontend/src/middleware.ts
+
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+
+/**
+ * Currently just a basic middleware that doesn't really do anything. Placeholder.
+ * I just put this here because I expect middleware to be a standard inclusion in NextJS projects.
+ */
+export function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+  if (pathname === '/home') {
+    return NextResponse.redirect(new URL(`/`, request.url));
+  }
+  return NextResponse.next();
+}
