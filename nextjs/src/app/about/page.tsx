35343866a46570212581a1bccefc61319d01d4e2
tbrendanj <tbrendanj@gmail.com>
2025-11-20 18:23:37 +0900

    [UNTESTED] switch home and about pages

--- nextjs/src/app/about/page.tsx
+++ nextjs/src/app/about/page.tsx
@@ -1,26 +1,17 @@
-'use client';
 import React from 'react';
-import { useState, useEffect } from 'react';
 import { fetchAbout } from '../utils/fetchAbout';
-import { BlocksContent } from '@strapi/blocks-react-renderer';
-import type { StrapiImage } from '../types/StrapiImage';
 import CommonPage from '@/components/CommonPage';
 
-export default function AboutPage() {
-  const [title, setTitle] = useState<string>('');
-  const [text, setText] = useState<BlocksContent>([]);
-  const [mainImage, setMainImage] = useState<StrapiImage | null>(null);
-  useEffect(() => {
-    const fetchAboutPageData = async () => {
-      const { data } = await fetchAbout();
-      setTitle(data.title);
-      setText(data.text);
-      setMainImage(data.main_image);
-    };
-    fetchAboutPageData();
-  }, []);
+export default async function AboutPage() {
+  const { data } = await fetchAbout();
+  console.log(data);
   return (
-    <CommonPage title={title} text={text} mainImage={mainImage} images={[]} />
+    <CommonPage
+      title={data.title}
+      text={data.text}
+      mainImage={data.mainImage}
+      images={[]}
+    />
   );
 }
 
--- nextjs/src/app/home/page.tsx
+++ nextjs/src/app/home/page.tsx
@@ -1,28 +1,18 @@
-'use client';
 import React from 'react';
-import { useState, useEffect } from 'react';
 import { fetchHome } from '../utils/fetchHome';
-import { BlocksContent } from '@strapi/blocks-react-renderer';
-import type { StrapiImage } from '../types/StrapiImage';
 
 import CommonPage from '@/components/CommonPage';
 import { Metadata } from 'next';
 
-export default function HomePage() {
-  const [title, setTitle] = useState<string>('');
-  const [text, setText] = useState<BlocksContent>([]);
-  const [mainImage, setMainImage] = useState<StrapiImage | null>(null);
-  useEffect(() => {
-    const fetchHomePageData = async () => {
-      const { data } = await fetchHome();
-      setTitle(data.title);
-      setText(data.text);
-      setMainImage(data.main_image);
-    };
-    fetchHomePageData();
-  }, []);
+export default async function HomePage() {
+  const { data } = await fetchHome();
   return (
-    <CommonPage title={title} text={text} mainImage={mainImage} images={[]} />
+    <CommonPage
+      title={data.title}
+      text={data.text}
+      mainImage={data.mainImage}
+      images={[]}
+    />
   );
 }
 
