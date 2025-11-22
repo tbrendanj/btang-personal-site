b2b85e874e761ef4ab0a2dfb0763ede658e4b125
tbrendanj <tbrendanj@gmail.com>
2025-11-22 11:28:00 +0900

    move comment

--- nextjs/src/app/globals/HeaderTabs.tsx
+++ nextjs/src/app/globals/HeaderTabs.tsx
@@ -1,5 +1,11 @@
 import { NavItemProps } from '@/components/NavItem';
 
+/**
+ * There's an argument that these are better off set in Strapi and requested,
+ * but since this repo is built simply for my personal use and I don't see
+ * these items changing often if at all, I've decided that it's better to just
+ * hardcode them and not make the extra requests to Strapi for my use case.
+ */
 const HeaderTabs: Array<NavItemProps> = [
   {
     href: '/',
--- nextjs/src/components/Header.tsx
+++ nextjs/src/components/Header.tsx
@@ -6,12 +6,6 @@ import HeaderTabs from '@/app/globals/HeaderTabs';
 import Link from 'next/link';
 
 export default function Header() {
-  /**
-   * There's an argument that these are better off set in Strapi and requested,
-   * but since this repo is built simply for my personal use and I don't see
-   * these items changing often if at all, I've decided that it's better to just
-   * hardcode them and not make the extra requests to Strapi for my use case.
-   */
   const title = process.env.siteName;
   const navItems: NavBarProps = {
     navItems: HeaderTabs,
