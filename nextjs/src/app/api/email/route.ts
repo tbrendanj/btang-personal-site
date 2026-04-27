d3d677c0d67fc981d30f6743f38a39384794ef79
tbrendanj <tbrendanj@gmail.com>
2026-04-27 16:11:16 +0900

    remove console log

--- nextjs/src/app/api/email/route.ts
+++ nextjs/src/app/api/email/route.ts
@@ -4,7 +4,6 @@ import Mail from 'nodemailer/lib/mailer';
 
 export async function POST(request: NextRequest) {
   const { email, name, message } = await request.json();
-  console.log(process.env.EMAIL_PASSWORD);
 
   const transport = nodemailer.createTransport({
     service: 'gmail',
