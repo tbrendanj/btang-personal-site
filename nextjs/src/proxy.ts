96886e1607c0a03e2a3534a784d71a6ba491f2cc
tbrendanj <tbrendanj@gmail.com>
2026-05-05 20:41:32 +0900

    proxy

--- nextjs/src/proxy.ts
+++ nextjs/src/proxy.ts
@@ -7,7 +7,7 @@ import { NextResponse } from 'next/server';
  * Currently just a basic middleware that doesn't really do anything other than redirect /home to /. Placeholder.
  * I just put this here because I expect middleware to be a standard inclusion in NextJS projects.
  */
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
   if (pathname === '/home') {
     return NextResponse.redirect(new URL(`/`, request.url));
