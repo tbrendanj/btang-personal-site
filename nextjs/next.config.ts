1093db966c0c54506ad6fa9b95ae72e88efb67c9
tbrendanj <tbrendanj@gmail.com>
2026-05-18 07:08:16 +0900

    deviceSizes

--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: NextConfig = {
   output: 'standalone',
   images: {
     dangerouslyAllowLocalIP: isDev,
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     remotePatterns: [
       {
         protocol: isDev ? 'http' : 'https',
