7fef9cbcf641afd2934d93395f9a6e0709c8038b
tbrendanj <tbrendanj@gmail.com>
2026-05-18 04:18:01 +0900

    turbopack root path

--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -27,6 +27,9 @@ const nextConfig: NextConfig = {
     siteName: "B. Tang's Internet Corner",
     siteDescription: 'The personal website of B. Tang.',
   },
+  turbopack: {
+    root: __dirname + '/..',
+  },
 };
 
 export default nextConfig;
