909f32f3a35cf8ace73087f4a0e2ac6345bf2836
tbrendanj <tbrendanj@gmail.com>
2026-05-18 07:47:18 +0900

    vercel still adding extra copy of domain; see if this will fix it

--- nextjs/src/app/utils/getStrapiImageUrl.tsx
+++ nextjs/src/app/utils/getStrapiImageUrl.tsx
@@ -1,4 +1,4 @@
 export function getStrapiImageUrl(url: string) {
   const baseUrl = process.env.STRAPI_MEDIA_URL ?? 'http://localhost:1337';
-  return baseUrl + url;
+  return url.includes(baseUrl) ? url : baseUrl + url;
 }
