89e2adcda77b38b7feb8bc1287c1924fc89b2735
tbrendanj <tbrendanj@gmail.com>
2026-07-24 01:24:53 +0900

    strapi prettier

--- strapi/src/api/blog-post/controllers/blog-post.ts
+++ strapi/src/api/blog-post/controllers/blog-post.ts
@@ -2,6 +2,6 @@
  * blog-post controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
 export default factories.createCoreController('api::blog-post.blog-post');
